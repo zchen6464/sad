ca688a8cee7da9f75c24d1273951592a56015c77
BT_1N3_31 <hkaur0745@bths.edu>
2019-03-06 11:41:17 -0500

    sdsd

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour { 
+
+    public float moveSpeed;
+    private Rigidbody playerRigidbody;
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+    private Camera mainCamera;
+
+    public Shooting pistol;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+        mainCamera = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * moveSpeed;
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if (groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 Look = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, Look, Color.blue);
+
+            transform.LookAt(new Vector3(Look.x,transform.position.y, Look.z));
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+            pistol.isFiring = true;
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            pistol.isFiring = false;
+        }
+    }
+    private void FixedUpdate()
+    {
+        playerRigidbody.velocity = moveVelocity;
+    }
+}
---
+++ Assets/Script/bulletScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour
+{
+
+    public float speed;
+    public float damage;
+    public string tag;
+    private Health health;
+    public Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set speed to "speed" in the direction of "up"
+        //the bullet moves where the pointy part is facing (given that it's rotate 90* in the x axis)
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = transform.up * speed;
+        
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Health>() != null)
+
+        {
+            //detects if the collision is with a enemy/damagable entity
+            //use GetComponent to access the script and thus the health
+            health = other.GetComponent<Health>();
+            health.hp = health.hp - damage;
+            if (health.hp <= 0)
+            {
+                Destroy(other.gameObject);
+            }
+        }
+        //destroys the bullet upon contact with another object/collider
+        Destroy(gameObject, 0.1f);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject, 3);
+    }
+}
